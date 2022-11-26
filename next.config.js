ab821705a98abce46aebfe1961b79caa00fa6278
Greg <wvitaliy8686@mail.ru>
2022-11-26 03:06:19 +0300

    damn all again

--- next.config.js
+++ next.config.js
@@ -2,6 +2,25 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  poweredByHeader: false,
+  env: {
+    APP_URL: process.env.REACT_APP_URL,
+  },
+  images: {
+    domains: ['localhost']
+  },
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'http://localhost4200/api/:path*'
+      },
+      {
+        source: '/uploads/:path*',
+        destination: 'http://localhost4200/uploads/:path*'
+      }
+    ]
+  }
 }
 
 module.exports = nextConfig
